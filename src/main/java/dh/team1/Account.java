b0ef74e0ec603f18434fc9d1cb82438ad29a9fd1
Stella Romeo <stellaromeo21@gmail.com>
2023-02-02 12:47:13 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set

--- src/main/java/dh/team1/Account.java
+++ src/main/java/dh/team1/Account.java
@@ -11,13 +11,51 @@ public class Account {
     private String genre;
 
     public Account(int id, String name,Data birthData, String bio, String genre){
-        this.id = id;
-        this.name = name;
-        this.birthData = birthData;
-        this.bio = bio;
-        this.genre = genre;
+        this.setId(id);
+        this.setName(name);
+        this.setBirthData(birthData);
+        this.setBio(bio);
+        this.setGenre(genre);
     }
 
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Data getBirthData() {
+        return birthData;
+    }
+
+    public void setBirthData(Data birthData) {
+        this.birthData = birthData;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
