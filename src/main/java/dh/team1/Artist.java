5b7c82b611684058f82f44b91af9cfc1fcef3a34
Stella Romeo <stellaromeo21@gmail.com>
2023-02-02 13:12:43 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set
    Creation class Playlist with variables to name and Song's list and creaton costructor method when I add a new ArrayList
    Change name class in Artist

--- src/main/java/dh/team1/Artist.java
+++ src/main/java/dh/team1/Artist.java
@@ -1,4 +1,61 @@
 package dh.team1;
 
+import javax.xml.crypto.Data;
+
 public class Artist {
+
+    private int id;
+    private String name;
+    private Data birthData;
+    private String bio;
+    private String genre;
+
+    public Account(int id, String name,Data birthData, String bio, String genre){
+        this.setId(id);
+        this.setName(name);
+        this.setBirthData(birthData);
+        this.setBio(bio);
+        this.setGenre(genre);
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Data getBirthData() {
+        return birthData;
+    }
+
+    public void setBirthData(Data birthData) {
+        this.birthData = birthData;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
