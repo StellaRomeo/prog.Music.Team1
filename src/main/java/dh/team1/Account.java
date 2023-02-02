1a865fb1dd7a01b52f70da108fc19c3276119834
Stella Romeo <stellaromeo21@gmail.com>
2023-02-02 15:49:15 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set
    Creation class Playlist with variables to name and Song's list and creaton costructor method when I add a new ArrayList
    Change name class in Artist
    Change name of costructor method in Artist
    Creation class Account with constructor method and new list playlist
    Add methods get/set id,usurname, password, user

--- src/main/java/dh/team1/Account.java
+++ src/main/java/dh/team1/Account.java
@@ -18,4 +18,37 @@ public class Account {
         this.user = user;
         this.playlists = new ArrayList<>();
     }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
