b07b8218ea337c6917b9fca1cd66be90ad453c69
Stella Romeo <stellaromeo21@gmail.com>
2023-02-03 11:25:25 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set
    Creation class Playlist with variables to name and Song's list and creaton costructor method when I add a new ArrayList
    Change name class in Artist
    Change name of costructor method in Artist
    Creation class Account with constructor method and new list playlist
    Add methods get/set id,usurname, password, user
    Add new value at the playlist, data creation, genre, user
    Creation class admin that extends class user and add variables ( id, username, password )
    Creation abstract class with music details. This class will be extended by "Song", "Playlist" and "Album"
    Modification to extend the MusicDetail abstract class
    Modification to extend the MusicDetail abstract class
    Modification class Account in an abstract class. This class will be extended by "User" and "Admin"
    Modification class Admin to extend Account abstract class
    Added extra value to Account class

--- src/main/java/dh/team1/Account.java
+++ src/main/java/dh/team1/Account.java
@@ -13,12 +13,37 @@ public abstract class Account {
 
     public Account(int id, String username, String password, String email){
         this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
+        this.setUsername(username);
+        this.setPassword(password);
+        this.setEmail(email);
         this.playlists = new ArrayList<>();
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
