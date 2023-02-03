50092c90086475ec3fc1cac4236d790369993c60
Stella Romeo <stellaromeo21@gmail.com>
2023-02-03 11:26:38 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set
    Creation class Playlist with variables to name and Song's list and creaton costructor method when I add a new ArrayList
    Change name class in Artist
    Change name of costructor method in Artist
    Creation class Account with constructor method and new list playlist
    Add methods get/set id,usurname, password, user
    Add new value at the playlist, data creation, genre, user
    Creation class admin that extends class user and add variables ( id, username, password )
    Creation abstract class with music details. This class will be extended by "Song", "Playlist" and "Album"
    Modification to extend the MusicDetail abstract class
    Modification to extend the MusicDetail abstract class
    Modification class Account in an abstract class. This class will be extended by "User" and "Admin"
    Modification class Admin to extend Account abstract class
    Added extra value to Account class
    Modification class User to extend Account abstract class plus extra value

--- src/main/java/dh/team1/User.java
+++ src/main/java/dh/team1/User.java
@@ -2,90 +2,29 @@ package dh.team1;
 
 import javax.xml.crypto.Data;
 
-public class User {
+public class User extends Account {
 
     private String name;
     private String surname;
-    private String email;
+    private Data dataBorn;
+    private String gender;
+    private String nationality;
     private String address;
     private String city;
     private String country;
-    private Data birthData;
-    private String gender;
 
-    public User (){
+
+
+    public User(int id, String username, String password, String email, String name,String surname, Data dataBorn,String gender,
+                String nationality, String address, String city, String country) {
+        super(id, username, password, email);
         this.name = name;
         this.surname = surname;
-        this.email = email;
+        this.dataBorn = dataBorn;
+        this.gender = gender;
+        this.nationality = nationality;
         this.address = address;
         this.city = city;
         this.country = country;
-        this.birthData = birthData;
-        this.gender = gender;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Data getBirthData() {
-        return birthData;
-    }
-
-    public void setBirthData(Data birthData) {
-        this.birthData = birthData;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 }
