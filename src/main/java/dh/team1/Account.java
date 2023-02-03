e913bb79b2872771bfcad511c3ae4e9e980965ff
Stella Romeo <stellaromeo21@gmail.com>
2023-02-03 12:04:46 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set
    Creation class Playlist with variables to name and Song's list and creaton costructor method when I add a new ArrayList
    Change name class in Artist
    Change name of costructor method in Artist
    Creation class Account with constructor method and new list playlist
    Add methods get/set id,usurname, password, user
    Add new value at the playlist, data creation, genre, user
    Creation class admin that extends class user and add variables ( id, username, password )
    Creation abstract class with music details. This class will be extended by "Song", "Playlist" and "Album"
    Modification to extend the MusicDetail abstract class
    Modification to extend the MusicDetail abstract class
    Modification class Account in an abstract class. This class will be extended by "User" and "Admin"
    Modification class Admin to extend Account abstract class
    Added extra value to Account class
    Modification class User to extend Account abstract class plus extra value
    Add methods get/set
    add commit
    Amount the Random method (UUID) is used in order to generate a unique identifier for each user so that there are no duplicates
    I eliminated the set methods because the basic account information such as username, email and password are established only once and cannot be changed

--- src/main/java/dh/team1/Account.java
+++ src/main/java/dh/team1/Account.java
@@ -14,9 +14,9 @@ public abstract class Account {
 
     public Account(int generateId, String username, String password, String email){
         this.id = generateId;
-        this.setUsername(username);
-        this.setPassword(password);
-        this.setEmail(email);
+        this.username = username;
+        this.password = password;
+        this.email = email;
         this.playlists = new ArrayList<>();
     }
 
@@ -28,23 +28,21 @@ public abstract class Account {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+
+    public List<Playlist> getPlaylists() {
+        return playlists;
     }
+
+
+
 }
