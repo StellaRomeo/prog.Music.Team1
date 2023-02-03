0fa3436d7b676495a29733dcf93a242ff39ffb42
Stella Romeo <stellaromeo21@gmail.com>
2023-02-03 10:58:12 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set
    Creation class Playlist with variables to name and Song's list and creaton costructor method when I add a new ArrayList
    Change name class in Artist
    Change name of costructor method in Artist
    Creation class Account with constructor method and new list playlist
    Add methods get/set id,usurname, password, user
    Add new value at the playlist, data creation, genre, user
    Creation class admin that extends class user and add variables ( id, username, password )
    Creation abstract class with music details. This class will be extended by "Song", "Playlist" and "Album"
    Modification to extend the MusicDetail abstract class
    Modification to extend the MusicDetail abstract class
    Modification class Account in an abstract class. This class will be extended by "User" and "Admin"

--- src/main/java/dh/team1/Account.java
+++ src/main/java/dh/team1/Account.java
@@ -3,52 +3,22 @@ package dh.team1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Account {
+public abstract class Account {
 
     private int id;
     private String username;
     private String password;
-    private User user;
+    private String email;
     private List<Playlist>playlists;
 
-    public Account(int id, String username, String password, User user){
+    public Account(int id, String username, String password, String email){
         this.id = id;
         this.username = username;
         this.password = password;
-        this.user = user;
+        this.email = email;
         this.playlists = new ArrayList<>();
     }
 
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
