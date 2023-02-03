b06787de13707f6381948afca036454fc7fffe05
Stella Romeo <stellaromeo21@gmail.com>
2023-02-03 12:33:16 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set
    Creation class Playlist with variables to name and Song's list and creaton costructor method when I add a new ArrayList
    Change name class in Artist
    Change name of costructor method in Artist
    Creation class Account with constructor method and new list playlist
    Add methods get/set id,usurname, password, user
    Add new value at the playlist, data creation, genre, user
    Creation class admin that extends class user and add variables ( id, username, password )
    Creation abstract class with music details. This class will be extended by "Song", "Playlist" and "Album"
    Modification to extend the MusicDetail abstract class
    Modification to extend the MusicDetail abstract class
    Modification class Account in an abstract class. This class will be extended by "User" and "Admin"
    Modification class Admin to extend Account abstract class
    Added extra value to Account class
    Modification class User to extend Account abstract class plus extra value
    Add methods get/set
    add commit
    Amount the Random method (UUID) is used in order to generate a unique identifier for each user so that there are no duplicates
    I eliminated the set methods because the basic account information such as username, email and password are established only once and cannot be changed
    Add methods to add and remove the playlist
    Creation constructor method

--- src/main/java/dh/team1/GenreMusical.java
+++ src/main/java/dh/team1/GenreMusical.java
@@ -1,9 +1,15 @@
 package dh.team1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenreMusical {
 
     private String name;
     private List<Song>songs;
+
+    public GenreMusical(String name){
+        this.name = name;
+        this.songs = new ArrayList<>();
+    }
 }
