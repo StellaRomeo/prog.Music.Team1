ab76eaf31843e4986653e9a41ec0274755d33313
Stella Romeo <stellaromeo21@gmail.com>
2023-02-04 13:44:32 +0100

    Creation title,artist and  time. Creation constructor method Creation method to listen to music
    Creation class user with constructor method and get/set
    Creation class account with constructor method and get/set
    Creation class Playlist with variables to name and Song's list and creaton costructor method when I add a new ArrayList
    Change name class in Artist
    Change name of costructor method in Artist
    Creation class Account with constructor method and new list playlist
    Add methods get/set id,usurname, password, user
    Add new value at the playlist, data creation, genre, user
    Creation class admin that extends class user and add variables ( id, username, password )
    Creation abstract class with music details. This class will be extended by "Song", "Playlist" and "Album"
    Modification to extend the MusicDetail abstract class
    Modification to extend the MusicDetail abstract class
    Modification class Account in an abstract class. This class will be extended by "User" and "Admin"
    Modification class Admin to extend Account abstract class
    Added extra value to Account class
    Modification class User to extend Account abstract class plus extra value
    Add methods get/set
    add commit
    Amount the Random method (UUID) is used in order to generate a unique identifier for each user so that there are no duplicates
    I eliminated the set methods because the basic account information such as username, email and password are established only once and cannot be changed
    Add methods to add and remove the playlist
    Creation constructor method
    Modification id. Inserted random method inside the constructor method.
    Creation packaging to separate contents, User and Music
    Creation interface class with method to play music.
    Add method to stop music.
    Change
    Change
    Add method to skip music.
    Add method to pause music.
    Add method to repeat track.
    Add method to shuffle to repeat randomly.
    Add method to add to playlist.
    Add method to remove to playlist.
    Add method to create playlist.
    Add method to like a song or album.
    Add method to search one song, album or playlist.
    Add method to dislike a song or album.
    Name change to make it more generic.
    Change in protected
    add methods set to emai, password
    Change variable in to String's Array
    Converte id da int a String to use UUID and Change String name.
    Change ArrayList
    Delete public.
    Create interface class and Add methods to create Account.
    Add methods to delete Account.
    Add methods to update Account.
    implement interface class AccountOperation in to abstract class Account
    implement Override methods
    Add methods get/set

--- src/main/java/Music/MusicDetail.java
+++ src/main/java/Music/MusicDetail.java
@@ -15,11 +15,52 @@ public abstract class MusicDetail {    //song-playlist-album
 
 
     public MusicDetail(String title, Artist artist, GenreMusical genreMusical, int duration, Data dataCreation, int assessment) {
-        this.title = title;
-        this.artist = artist;
-        this.genreMusical = genreMusical;
-        this.duration = duration;
-        this.dataCreation = dataCreation;
+        this.setTitle(title);
+        this.setArtist(artist);
+        this.setGenreMusical(genreMusical);
+        this.setDuration(duration);
+        this.setDataCreation(dataCreation);
         this.assessment = assessment;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public GenreMusical getGenreMusical() {
+        return genreMusical;
+    }
+
+    public void setGenreMusical(GenreMusical genreMusical) {
+        this.genreMusical = genreMusical;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Data getDataCreation() {
+        return dataCreation;
+    }
+
+    public void setDataCreation(Data dataCreation) {
+        this.dataCreation = dataCreation;
     }
 }
